8ae9845bec05efffca258631515d890cc19bc1ca
Colton Weber <shrike76@aim.com>
2019-10-20 19:21:11 -0500

    Skeleton Class

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private String firstname;
+    private String lastname;
+    private int ID;
+    private String classStanding;
+    private String email;
+    private String addressLine;
+    private String city;
+    private char state;
+    private int zipcode;
+    public Student(String a, String b, int c, String d, String e, String f, String g, char h, int i){
+        firstname=a;
+        lastname=b;
+        ID=c;
+        classStanding=d;
+        email=e;
+        addressLine=f;
+        city=g;
+        state=h;
+        zipcode=i;
+    }
+}
