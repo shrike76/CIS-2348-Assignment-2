482fb87fead5f59db834b78048bb9a7d53cb914d
Colton Weber <shrike76@aim.com>
2019-10-21 18:35:11 -0500

    Added more of the skeleton. Revamped how the objects get data. Hopefully I'm doing this properly.

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.regex.*;
 public class Student {
     private String firstname;
     private String lastname;
@@ -6,8 +8,9 @@ public class Student {
     private String email;
     private String addressLine;
     private String city;
-    private char state;
+    private String state;
     private int zipcode;
+    private ArrayList<Student> studentarray;
     /*public Student(String a, String b, int c, String d, String e, String f, String g, char h, int i){
         firstname=a;
         lastname=b;
@@ -19,6 +22,13 @@ public class Student {
         state=h;
         zipcode=i;
     }*/
+
+    public Student(){}
+
+    public ArrayList getArray() {
+        return studentarray;
+    }
+
     public void firstname_(String a){
         char[] b = a.toCharArray();
         for (char c : b) {
@@ -28,7 +38,6 @@ public class Student {
             }
         }
         firstname=a;
-        System.out.println(firstname);
     }
     public void lastname_(String a){
         char[] b = a.toCharArray();
@@ -39,17 +48,57 @@ public class Student {
             }
         }
         lastname=a;
-        System.out.println(lastname);
     }
     public void ID_(int a){
-        if (a>7){
-            System.out.println("Error");
+        if (a>9999999){
+            System.out.println("Error, greater than 7");
             System.exit(0);
         }
         else if (!(a==(int)a)){
-            System.out.println("Error");
+            System.out.println("Error, not an integer");
             System.exit(0);
         }
         ID=a;
+
     }
+    public void classstanding_(String a){ //fix this
+        if(a=="freshman"||a=="sophomore"||a=="junior"||a=="senior"){
+            classStanding=a;
+        }
+        else {
+            System.out.println("Error");
+            System.exit(0);
+        }
     }
+    public static boolean email_(String email) //fix this
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+    public void addressLine_(String a){
+        addressLine=a;
+    }
+    public void city_(String a){
+        char[] b = a.toCharArray();
+        for (char c : b) {
+            if(!Character.isLetter(c)) {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+        city=a;
+    }
+    public void state_(String a){
+        state=a;
+    }
+    public void zipcode_(int a){
+        zipcode=a;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -4,27 +4,44 @@ public class main {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the number of students' data you with to enter:");
         int n=input.nextInt();
-        Student[] s=new Student[n];
 
         for (int i=0;i<n;i++){
-            Student s1=new Student();
+            List<Student> studentarray = new ArrayList<Student>();
+            studentarray.add(new Student());
             System.out.println("Enter the first name:");
             String a=input.next();
-            s1.firstname_(a);
+            studentarray.get(i).firstname_(a);
             System.out.println("Enter the last name:");
             String b=input.next();
-            s1.lastname_(b);
+            studentarray.get(i).lastname_(b);
             System.out.println("Enter the ID:");
             int c=input.nextInt();
-            s1.ID_(c);
-
-
-            }
-
-
-
+            studentarray.get(i).ID_(c);
+            System.out.println("Enter the class standing:");
+            String d=input.next();
+            studentarray.get(i).classstanding_(d);
+            System.out.println("Enter the email:");
+            String f=input.next();
+            studentarray.get(i).email_(f);
+            System.out.println("Enter the address line:");
+            String e=input.next();
+            studentarray.get(i).addressLine_(e);
+            System.out.println("Enter the City:");
+            String g=input.next();
+            studentarray.get(i).city_(g);
+            System.out.println("Enter the State:");
+            String h=input.next();
+            studentarray.get(i).state_(h);
+            System.out.println("Enter the Zip code:");
+            int j=input.nextInt();
+            studentarray.get(i).zipcode_(j);
+            i++;
         }
 
 
+
     }
+
+
+}
 
