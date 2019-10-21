eab3b5db4034c6973a6dfbaf35233c0419914cb4
Colton Weber <shrike76@aim.com>
2019-10-20 19:16:52 -0500

    Skeleton

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args){
+        int n;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number of students' data you with to enter:");
+        n=input.nextInt();
+        Student[] s=new Student[n];
+
+
+
+    }
+}
