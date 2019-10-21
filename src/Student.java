351137c66dd49f5b78adba15a899e8ad6db7a4f8
Colton Weber <shrike76@aim.com>
2019-10-21 01:14:23 -0500

    Bunch of stuff and some rewrites

--- src/Student.java
+++ src/Student.java
@@ -8,7 +8,7 @@ public class Student {
     private String city;
     private char state;
     private int zipcode;
-    public Student(String a, String b, int c, String d, String e, String f, String g, char h, int i){
+    /*public Student(String a, String b, int c, String d, String e, String f, String g, char h, int i){
         firstname=a;
         lastname=b;
         ID=c;
@@ -18,5 +18,38 @@ public class Student {
         city=g;
         state=h;
         zipcode=i;
+    }*/
+    public void firstname_(String a){
+        char[] b = a.toCharArray();
+        for (char c : b) {
+            if(!Character.isLetter(c)) {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+        firstname=a;
+        System.out.println(firstname);
     }
-}
+    public void lastname_(String a){
+        char[] b = a.toCharArray();
+        for (char c : b) {
+            if(!Character.isLetter(c)) {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+        lastname=a;
+        System.out.println(lastname);
+    }
+    public void ID_(int a){
+        if (a>7){
+            System.out.println("Error");
+            System.exit(0);
+        }
+        else if (!(a==(int)a)){
+            System.out.println("Error");
+            System.exit(0);
+        }
+        ID=a;
+    }
+    }
--- src/main.java
+++ src/main.java
@@ -1,13 +1,30 @@
 import java.util.*;
 public class main {
     public static void main(String[] args){
-        int n;
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the number of students' data you with to enter:");
-        n=input.nextInt();
+        int n=input.nextInt();
         Student[] s=new Student[n];
 
+        for (int i=0;i<n;i++){
+            Student s1=new Student();
+            System.out.println("Enter the first name:");
+            String a=input.next();
+            s1.firstname_(a);
+            System.out.println("Enter the last name:");
+            String b=input.next();
+            s1.lastname_(b);
+            System.out.println("Enter the ID:");
+            int c=input.nextInt();
+            s1.ID_(c);
+
+
+            }
+
+
+
+        }
 
 
     }
-}
+
